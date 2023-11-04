ca54fec53592be36f0f167e7f039cdf87fc5d72a
opipi406 <opipi.work@gmail.com>
2023-11-04 10:57:44 +0900

    add: typescript導入

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: "resources/js/app.ts",
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+});
